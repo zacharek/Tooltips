59b168fafdfdfa973b3509d5f7fdb8359b96c1e5
Szymon.Zacharewicz <szymon.zacharewicz@gmail.com>
2019-11-12 19:38:42 +0100

    added js, no styling

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
-/*
- <span class="tooltipText">Text tooltipa</span>
- */
+let allToolTips = document.querySelectorAll('.tooltip');
+
+allToolTips.forEach(function (tooltip) {
+    tooltip.addEventListener('mouseover', function() {
+        let toolTipText = this.dataset.text;
+        let newSpan = document.createElement('span');
+        newSpan.className = 'tooltipText';
+        newSpan.innerText = toolTipText;
+        this.appendChild(newSpan);
+    })
+});
+
+allToolTips.forEach(function (tooltip) {
+    tooltip.addEventListener('mouseout', function() {
+        let span = document.querySelector('.tooltipText');
+        this.removeChild(span);
+    })
+});
